c0b96d3eb4607c8f21df7808f337f38a2b9eb486
kazuma-0 <as8644459@gmail.com>
2022-06-02 19:09:26 +0530

    add / to public path

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "vue-auto-deploy"
+  publicPath: "/vue-auto-deploy/"
 })
