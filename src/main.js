7f71829c2f89a4a9d583c565c716507fbb2dd856
mayc01 <yanchaoma@foxmail.com>
2023-03-27 18:57:05 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import jquery from 'jquery'
-import * as d3 from 'd3'
+import 'jquery'
 
 createApp(App).mount('#app')
-createApp(App).prototype.$ = jquery
-createApp(App).prototype.d3 = d3
+
--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,17 @@
+const webpack = require('webpack')
 const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
     transpileDependencies: true,
     publicPath: "/graph-qabot/",
     configureWebpack: {
-        externals: {
-            jquery: "jquery"
-        }
-    }
+        plugins: [
+            new webpack.ProvidePlugin({
+                $: "jquery",
+                jQuery: "jquery",
+                jquery: "jquery",
+                "window.jQuery": "jquery"
+            })
+        ],
+    },
 })
 
