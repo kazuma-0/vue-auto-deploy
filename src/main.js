f986f504a8d5e6f9f622ca01929c2d73aa77b9db
mayc01 <yanchaoma@foxmail.com>
2023-03-27 17:43:47 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import jquery from 'jquery'
+import * as d3 from 'd3'
 
 createApp(App).mount('#app')
+Vue.prototype.$ = jquery
+Vue.prototype.d3 = d3
